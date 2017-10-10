2bbaa5911a2fcbcf50d7c29f1fc7098dfb797535
qiaosiyi <qsy2011815@163.com>
2017-10-10 17:41:15 +0800

    aac

--- 3000fps/fatigueDetect.cpp
+++ 3000fps/fatigueDetect.cpp
@@ -298,7 +298,7 @@ int fatiguepredict(cv::Mat &frame, cv::CascadeClassifier &fcc, LBFRegressor &reg
 					
 					cout << "PERCLOSE: " << perclose << endl;
 					perclose_color = cv::Scalar(0,255,0);
-					if (perclose > 0.5){
+					if (perclose > 0.25){
 						return 1;
 					}
 					if (perclose > percloseThreshold)
